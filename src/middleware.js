f117a3f2b1369df82dc698530eaa003f4ecb7930
Flivian91 <fliviandev@gmail.com>
2025-02-17 05:01:54 +0300

    commit

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/admin(.*)",
-  "/upload(.*)",
-  "/(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/upload(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
