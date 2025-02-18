9fb5f7bb784ec9eaf6a9fd6bf9e4b5cfec85463b
Flivian91 <fliviandev@gmail.com>
2025-02-18 11:52:50 +0300

    commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -77,6 +77,7 @@ export default function Header() {
       {sidebarOpen && <Overlay onClose={() => setSidebarOpen(false)} />}
       {/* Search Input Model */}
       <SearchInputModel />
+      <Overlay/>
     </div>
   );
 }
--- src/Components/models/SearchInputModel.jsx
+++ src/Components/models/SearchInputModel.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaTimes } from "react-icons/fa";
 
 function SearchInputModel() {
   return (
-    <div className='fixed top-20 left-1/2 transform -translate-x-1/2 bg-white w-1/2 z-50  shadow-md'>
+    <div className="fixed top-20 left-1/2 transform -translate-x-1/2  w-full border overflow-hidden  md:w-1/2 z-50 rounded-lg shadow-md">
       {/* Search Area */}
-      <div>
+      <div className="bg-white w-full py-2 px-6 flex items-center gap-2 md:gap-4">
         <button>
           <AiOutlineSearch size={20} />
+        </button>
+        <input
+          type="text"
+          placeholder="Search Talents"
+          className="flex-1 w-full text-lg px-2 py-0 border-none outline-none focus:outline-none"
+        />
+        <button>
+        <FaTimes />
         </button>
       </div>
       {/* Suggestion results Area */}
     </div>
-  )
+  );
 }
 
-export default SearchInputModel
+export default SearchInputModel;
